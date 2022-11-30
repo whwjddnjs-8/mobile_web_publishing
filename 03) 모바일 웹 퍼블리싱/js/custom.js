9d05e7ad5c4f757845583288dda4dcd15f03cbf9
조정원 <whwjddnjs8@gmail.com>
2022-11-30 16:39:12 +0900

    08 - Search

--- 03) 모바일 웹 퍼블리싱/js/custom.js
+++ 03) 모바일 웹 퍼블리싱/js/custom.js
@@ -11,4 +11,13 @@ $(function () {
 		$(this).siblings('.category-accordion .detail').stop().slideUp()
 		$(this).next().stop().slideDown()
 	})
+
+	// Recent Search Result
+	$('.search-recent .btn-clear').click(function () {
+		$(this).parent('.item').hide()
+	})
+
+	$('.search-recent .btn-all-clear').click(function () {
+		$('.search-recent .item').hide()
+	})
 })
