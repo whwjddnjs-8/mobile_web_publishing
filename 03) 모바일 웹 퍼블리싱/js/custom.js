a8031dbbd3408f439530784de85b7a1248760943
조정원 <whwjddnjs8@gmail.com>
2022-12-02 14:49:52 +0900

    04 - Main

--- 03) 모바일 웹 퍼블리싱/js/custom.js
+++ 03) 모바일 웹 퍼블리싱/js/custom.js
@@ -1,9 +1,9 @@
 $(function () {
-	//Include html Files
+	/* Include html Files */
 	$('.gnb-include').load('/include/gnb.html')
 	$('.main-header-include').load('/include/main-header.html')
 
-	// Category Accordion
+	/* Category Accordion */
 	$('.category-accordion .detail').eq(0).show()
 	$('.category-accordion .title').click(function () {
 		$(this).addClass('active')
@@ -12,7 +12,7 @@ $(function () {
 		$(this).next().stop().slideDown()
 	})
 
-	// Recent Search Result
+	/* Recent Search Result */
 	$('.search-recent .btn-clear').click(function () {
 		$(this).parent('.item').hide()
 	})
@@ -21,11 +21,38 @@ $(function () {
 		$('.search-recent .item').hide()
 	})
 
-	// Cart Items
+	/* Cart Items */
 	$('.cart-content .btn-all-clear').click(function () {
 		$('.cart-item').hide()
 	})
 	$('.cart-item-title .btn-clear').click(function () {
 		$(this).parent().parent().hide()
+	})
+
+	/* Front Slider */
+	$('.front-slider').slick({
+		slidesToShow: 1,
+		dots: false,
+		arrows: true,
+		autoplay: false,
+  		autoplaySpeed: 2000,
+	});
+
+	/* Goods Suggestion Slider */
+	$('.goods-suggestion-items').slick({
+		slidesToShow: 3,
+		slidesToScroll: 2,
+		dots: false,
+		arrows: false,
+	})
+
+	/* Wish */
+	$('.wish').click(function () {
+		$(this).toggleClass('active')
+	})
+
+	/* Front Footer */
+	$('.btn-view-more').click(function () {
+		$('.company-info-more').slideToggle()
 	})
 })
